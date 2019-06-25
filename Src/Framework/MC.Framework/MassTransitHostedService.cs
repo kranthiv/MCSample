0c44ae862452dbe8619548cb7565a5f8b77c11d6
kranthi veeramachaneni <kranthiv@live.com>
2019-06-26 00:05:35 +0100

    Add project files.

---
+++ Src/Framework/MC.Framework/MassTransitHostedService.cs
@@ -0,0 +1,62 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MC.Framework
+{
+    public class MassTransitHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly IApplicationLifetime _appLifetime;
+        private readonly IBusControl _busControl;
+        public MassTransitHostedService(ILogger<MassTransitHostedService> logger,IApplicationLifetime appLifetime,IBusControl busControl)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+            _busControl = busControl;
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStarted.Register(OnStarted);
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
+            await _busControl.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StopAsync(cancellationToken);
+        }
+
+        private void OnStarted()
+        {
+            _logger.LogInformation("OnStarted has been called.");
+
+            // Perform post-startup activities here
+        }
+
+        private void OnStopping()
+        {
+            _logger.LogInformation("OnStopping has been called.");
+            Dispose();
+            // Perform on-stopping activities here
+        }
+
+        private void OnStopped()
+        {
+            _logger.LogInformation("OnStopped has been called.");
+
+            // Perform post-stopped activities here
+        }
+
+        public void Dispose()
+        {
+            _busControl?.Stop();
+        }
+    }
+}
---
+++ Src/Hosting/MC.ServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using MC.Framework;
+using MC.Framework.WindowsService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace MC.ServiceHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MassTransitHostedService>();
+                })
+                .UseWindowsService()
+                .Build();
+
+            await host.RunAsync().
+                ConfigureAwait(false);
+
+        }
+    }
+}
